786c12e58050006958f6aef0e6385060da5458ee
Stephen Nutbrown <steveswfc@gmail.com>
2020-10-19 13:12:16 +0100

    Add slf4j2 example

---
+++ slf4j2-example-all-default/src/main/java/com/example/demo/controllers/HealthController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/health")
+
+public class HealthController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HealthController.class);
+
+    @GetMapping
+    public ResponseEntity<String> checkHealth(){
+        LOG.info("My message set at info level");
+        return ResponseEntity.ok("Healthy");
+    }
+}
